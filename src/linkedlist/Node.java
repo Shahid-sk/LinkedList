b51803f75b79d83dec64fae72fd988d941b81bc0
Shahid-sk <mail2sk.shahid@gmail.com>
2021-07-18 15:38:02 +0530

    added: inserted 40 after 30 to the list

--- src/linkedlist/Node.java
+++ src/linkedlist/Node.java
@@ -7,12 +7,11 @@ public class Node {
 		LinkedList linkedList = new LinkedList();
 		linkedList.insert(56);
 		linkedList.insert(30);
+		linkedList.insert(40);
 		linkedList.insert(70);
 		linkedList.print();
 		linkedList.searchNode(30);
 
-		int poplast = linkedList.poplast();
-		System.out.println(poplast);
 		linkedList.print();
 
 	}
